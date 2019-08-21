f2a6bf7cab8336e4f0295d75d5393bf55099e4ea
Morgan <lockhart.mb@gmail.com>
2019-08-20 22:35:41 -0400

    Created Timer component with placeholder text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Timer from "./components/Timer";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Countdown</h1>
+        <Timer />
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Timer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Timer = () => {
+  return <h2>hello</h2>;
+};
+
+export default Timer;
